f14fca5b1c3c36c8c84a938773482c098a09b881
Guilhermecamposss <116971414+Guilhermecamposss@users.noreply.github.com>
2022-11-07 21:58:53 -0300

    Add files via upload

---
+++ pilhadinamica/pilhadinamica.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 1000;
+
+typedef struct{
+    int chave;
+    int nome[30];
+}TipoItem;
+
+struct noh{
+    TipoItem item;
+    struct noh *prox;
+};
+
+typedef struct noh TipoNoh;
+
+typedef struct{
+    TipoNoh *topo;
+}TipoPilha;
+
+void inicializaPilha(TipoPilha*);
+void destroiPilha(TipoPilha*);
+int pilhaVazia(TipoPilha*);
+int empilha(TipoPilha*,TipoItem);
+int desempilha(TipoPilha*,TipoItem*);
+void imprimeItem(TipoItem);
+int imprimePilha(TipoPilha*);
